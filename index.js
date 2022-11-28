fdfc03c848a18f427f8077904c7014f5d3c0d4b4
dsdeloar02 <dsdeloar02@gmail.com>
2022-11-28 17:14:59 +0600

    all buyer route added

--- index.js
+++ index.js
@@ -109,6 +109,14 @@ async function run(){
             res.send(result)
         })
 
+        app.delete('/buyer/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await usersCollection.deleteOne(query)
+            console.log(result)
+            res.send(result)
+        })
+
        
 
         app.get('/products', async (req, res) => {  
